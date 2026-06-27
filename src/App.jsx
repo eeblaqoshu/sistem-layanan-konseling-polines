7eda3d933fd13decc14eeb2c020cd0785369e12b
duta_error <mqolbishufi@gmail.com>
2026-06-27 14:51:16 +0700

    membuat projek ui/ux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Lorem from './page/lorem'
+
+function App() {
+
+  return (
+    <>
+    <Lorem />
+    </>
+  )
+}
+
+export default App
---
+++ src/page/Lorem.jsx
@@ -0,0 +1,5 @@
+export default function Lorem() {
+    return (
+        <div className="text-center text-white text-lg">Hello World</div>
+    )
+}
